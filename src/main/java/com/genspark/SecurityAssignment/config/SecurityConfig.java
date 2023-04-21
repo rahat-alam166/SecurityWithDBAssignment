861e11a0c8e0ffa4756f578d2202352607245c4a
Rahat Alam <rahat.alam519@gmail.com>
2023-04-21 08:37:42 -0500

    Final Security implementation with database authentication. Can add new users to database via post home/add in format of :     "username":"admin",     "password":"admin",     "role":"ROLE_ADMIN"

--- src/main/java/com/genspark/SecurityAssignment/config/SecurityConfig.java
+++ src/main/java/com/genspark/SecurityAssignment/config/SecurityConfig.java
@@ -14,7 +14,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-//test
+
 @Configuration
 public class SecurityConfig
 {
